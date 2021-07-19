fe174332472ebb3537d14ae1a16a43f74534b7a3
Janhavi Tatewar <janhavi.tatewar@bhnetwork.com>
2021-07-19 21:52:11 +0530

    Updates

---
+++ precache-manifest.c8bee022d4cab51a3286e9cc22570218.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94be26f18779931033ec",
+    "url": "/Portfolio/static/css/main.7d7df12b.chunk.css"
+  },
+  {
+    "revision": "94be26f18779931033ec",
+    "url": "/Portfolio/static/js/main.94be26f1.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "66efc7d10c4fd7026c16b88783a8c3de",
+    "url": "/Portfolio/index.html"
+  }
+];
